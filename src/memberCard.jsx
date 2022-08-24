02a87dbad36123e6182a887cf5cff02c2634b519
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2022-08-24 11:08:19 +0200

    made card for company members, memberData, imported pictures

---
+++ src/memberCard.jsx
@@ -0,0 +1,16 @@
+import { memberData } from "./memberData";
+
+function MemberCard() {
+  return (
+    <div>
+      {memberData.map((member) => (
+        <div key={member.id}>
+          <img src={member.img} alt={member.name}></img>  
+          <p>{member.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default MemberCard;
