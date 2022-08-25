0223da3ff2990a4612aa9f09c4581b06f5b9e50f
Jonatan Nyberg <33462941+nybbe123@users.noreply.github.com>
2022-08-25 21:40:17 +0200

    validation

--- src/components/layout/login/Login.js
+++ src/components/layout/login/Login.js
@@ -1,12 +1,40 @@
 import React from "react";
 import style from "./Login.module.css";
 import Button from "../../UI/Button";
-import Input from "../../UI/Input";
-import Label from "../../UI/Label";
+import { useFormik } from "formik";
+
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.email) {
+    errors.email = "Required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Invalid email address";
+  }
+
+  if (!values.password) {
+    errors.password = "Required";
+  } else if (values.password.length < 6) {
+    errors.password = "Must be atleast 6 characters";
+  }
+
+  return errors;
+};
 
 const Login = (props) => {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validate,
+    onSubmit: (values) => {
+      localStorage.setItem("user", JSON.stringify(values));
+    },
+  });
+
   return (
-    <form className={style["form-container"]}>
+    <form onSubmit={formik.handleSubmit} className={style["form-container"]}>
       <div>
         <h2>Welcome to webly</h2>
         <p className={style["form-sub-title"]}>
@@ -14,17 +42,39 @@ const Login = (props) => {
         </p>
       </div>
       <div className={style["form-input-container"]}>
-        <Label htmlFor="email">Email*</Label>
-        <Input type="text" />
+        <label htmlFor="email">Email*</label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
+        />
+        {formik.touched.email && formik.errors.email ? (
+          <div className={style["error-container"]}>{formik.errors.email}</div>
+        ) : null}
       </div>
       <div className={style["form-input-container"]}>
-        <Label htmlFor="password">Password*</Label>
-        <Input type="password" />
+        <label htmlFor="password">Password*</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.password}
+        />
+        {formik.touched.password && formik.errors.password ? (
+          <div className={style["error-container"]}>
+            {formik.errors.password}
+          </div>
+        ) : null}
         <ul>
           <li>Forgot password?</li>
         </ul>
       </div>
-      <Button>LOGIN</Button>
+      <Button type={"submit"}>LOGIN</Button>
       <div className={style["sign-up-in"]}>
         <p>
           Dont have an account?{" "}
--- src/components/layout/login/Register.js
+++ src/components/layout/login/Register.js
@@ -1,12 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import style from "./Register.module.css";
 import Button from "../../UI/Button";
-import Input from "../../UI/Input";
-import Label from "../../UI/Label";
+import { useFormik } from "formik";
+
+const validate = (values) => {
+  const errors = {};
+
+  if (!values.name) {
+    errors.name = "Required";
+  } else if (values.name.length > 15) {
+    errors.name = "Must be 15 characters or less";
+  } else if (values.name.length < 2) {
+    errors.name = "Must be atleast 2 characters";
+  }
+
+  if (!values.email) {
+    errors.email = "Required";
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = "Invalid email address";
+  }
+
+  if (!values.password) {
+    errors.password = "Required";
+  } else if (values.password.length < 6) {
+    errors.password = "Must be atleast 6 characters";
+  }
+
+  return errors;
+};
 
 const Register = (props) => {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+    validate,
+    onSubmit: (values) => {
+      localStorage.setItem("user", JSON.stringify(values));
+    },
+  });
+
   return (
-    <form className={style["form-container"]}>
+    <form className={style["form-container"]} onSubmit={formik.handleSubmit}>
       <div>
         <h2>Sign up to Webly</h2>
         <p className={style["form-sub-title"]}>
@@ -14,26 +51,63 @@ const Register = (props) => {
         </p>
       </div>
       <div className={style["form-input-container"]}>
-        <Label htmlFor="email">Email*</Label>
-        <Input type="text" />
+        <label htmlFor="name">First & Last name*</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.name}
+        />
+        {formik.touched.name && formik.errors.name ? (
+          <div className={style["error-container"]}>{formik.errors.name}</div>
+        ) : null}
       </div>
       <div className={style["form-input-container"]}>
-        <Label htmlFor="password">Password*</Label>
-        <Input type="password" />
-        <ul>
-          <li>Forgot password?</li>
-        </ul>
+        <label htmlFor="email">Email*</label>
+        <input
+          type="text"
+          id="email"
+          name="email"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.email}
+        />
+        {formik.touched.email && formik.errors.email ? (
+          <div className={style["error-container"]}>{formik.errors.email}</div>
+        ) : null}
       </div>
-      <Button>LOGIN</Button>
+      <div className={style["form-input-container"]}>
+        <label htmlFor="password">Password*</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.password}
+        />
+        {formik.touched.password && formik.errors.password ? (
+          <div className={style["error-container"]}>
+            {formik.errors.password}
+          </div>
+        ) : null}
+        <div className={style["terms-conditions"]}>
+          <input type="checkbox" />
+          <span>I agree to terms & conditions</span>
+        </div>
+      </div>
+      <Button type={"submit"}>SIGN UP</Button>
       <div className={style["sign-up-in"]}>
         <p>
-          Dont have an account?{" "}
+          Already have an account?{" "}
           <span
             onClick={() => {
               props.signUpInHandler(true);
             }}
           >
-            Sign up
+            Sign in
           </span>
         </p>
       </div>
