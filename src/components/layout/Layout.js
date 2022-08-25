49a19ac78f72a8141de8f674d384350aad40cf74
Millie Cheung <cheungmillie9@gmail.com>
2022-08-25 15:30:58 +0200

    static layout for contact, form and formik not inserted

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import CompanyMap from "./companyMap/CompanyMap";
+import ContactPage from "./contact/contact-page/ContactPage";
 import Footer from "./footer/Footer";
 import Header from "./header/Header";
 
@@ -9,6 +10,7 @@ const Layout = () => {
     <div>
       <Header />
       <Outlet />
+      <ContactPage />
       <CompanyMap />
       <Footer />
     </div>
---
+++ src/components/layout/contact/contact-form/ContactForm.js
@@ -0,0 +1,7 @@
+import style from "./ContactForm.module.css";
+
+const ContactForm = () => {
+  return <form id={style.root}>contact form</form>;
+};
+
+export default ContactForm;
