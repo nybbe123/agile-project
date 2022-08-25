43dfabaaedebb5e1596d1a87a0aaed5e2b40e76b
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-08-25 14:49:02 +0200

    created header

--- src/components/layout/header/Header.js
+++ src/components/layout/header/Header.js
@@ -1,5 +1,21 @@
+import style from "./Header.module.css";
+
 const Header = () => {
-  return <div style={{ background: "salmon" }}>This is Header</div>;
+  return (
+    <header className={style.header}>
+      <div className={style.headerLeft}>
+        <span className={style.logo}>WEBLY</span>
+      </div>
+      <nav className={style.headerRight}>
+        <ul className={style.navLinks}>
+          <li className={style.navItems}>SERVICES</li>
+          <li className={style.navItems}>ABOUT US</li>
+          <li className={style.navItems}>CONTACT</li>
+          <li className={style.navItems}>LOGIN</li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
