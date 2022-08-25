5ebbfc0dceb04e7dfaf13747f33e63e6959ca837
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2022-08-25 15:45:44 +0200

    imported new memberPics, added more info in memberData, style in member section

--- src/components/member/memberCard.js
+++ src/components/member/memberCard.js
@@ -1,14 +1,31 @@
 import { memberData } from "./memberData";
+import style from '../member/memberSection.module.css';
 
 function MemberCard() {
   return (
-    <div>
-      {memberData.map((member) => (
-        <div key={member.id}>
-          <img src={member.img} alt={member.name}></img>
-          <p>{member.name}</p>
-        </div>
-      ))}
+    <div className={style["container"]}>
+      <p className={style["p"]}>ABOUT US</p>
+      <div className={style["memberTitleContainer"]}>
+        <h2 className={style["theFantastic"]}>THE FANTASTIC</h2>
+        <h2 className={style["five"]}> FIVE</h2>
+      </div>
+      <div className={style["memberContainer"]}>
+        {memberData.map((member) => (
+          <div className={style["memberInfo"]} key={member.id}>
+            <img
+              className={style["memberPic"]}
+              src={member.img}
+              alt={member.name}
+            ></img>
+            <div className={style["memberText"]}>
+              <p className={style["memberName"]}>{member.name}</p>
+              <p>{member.title}</p>
+              <p>{member.phoneNmbr}</p>
+              <p>{member.email}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
