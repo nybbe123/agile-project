6f7e1f85162a89ea4421f3746137cb69a1663581
Erikisak <erik.isak@icloud.com>
2022-08-28 17:35:34 +0200

    service component created

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -2,12 +2,14 @@ import { Outlet } from "react-router-dom";
 import CompanyMap from "./companyMap/CompanyMap";
 import Footer from "./footer/Footer";
 import Header from "./header/Header";
+import Services from "./services/services";
 
 const Layout = () => {
   window.scrollTo(0, 0);
   return (
     <div>
       <Header />
+      <Services />
       <Outlet />
       <CompanyMap />
       <Footer />
