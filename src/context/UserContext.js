23cfac31f909d0486b896d220c1a8d552f7ae3fb
Jonatan Nyberg <33462941+nybbe123@users.noreply.github.com>
2022-08-29 15:20:16 +0200

    login/register - save to LS, validation

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,29 +1,85 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const UserContext = createContext({
+  users: [],
   isLoggedIn: false,
+  userExist: false,
+  incorrectInfo: false,
+  onRegister: () => {},
   onLogIn: () => {},
   onLogout: () => {},
 });
 
 export const UserContextProvider = (props) => {
+  const navigate = useNavigate();
+  const [users, setUsers] = useState([]);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userExist, setUserExist] = useState(false);
+  const [incorrectInfo, setIncorrectInfo] = useState(false);
 
-  const logInHandler = () => {
-    console.log("test");
+  const newUserHandler = (userData) => {
+    if (
+      users.length > 0 &&
+      users.find((user) => {
+        return user.id.trim() === userData.id.trim();
+      })
+    ) {
+      setUserExist(true);
+      return console.log("user exist");
+    } else {
+      setUsers((prevUsers) => {
+        return [...prevUsers, userData];
+      });
+      localStorage.setItem("users", JSON.stringify([...users, userData]));
+      localStorage.setItem("isLoggedIn", JSON.stringify(userData));
+    }
+    setUserExist(false);
+    setIsLoggedIn(true);
+    navigate("/");
+  };
+
+  const logInHandler = (userData) => {
+    if (
+      users.length > 0 &&
+      users.find((user) => user.id === userData.email) &&
+      users.find((user) => user.password === userData.password.toString())
+    ) {
+      setIncorrectInfo(false);
+      localStorage.setItem("isLoggedIn", JSON.stringify(userData));
+      navigate("/");
+    } else {
+      console.log("Incorrect!", userData.email, userData.password);
+      setIncorrectInfo(true);
+    }
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
-    console.log("test");
-    localStorage.removeItem("user");
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
+
+  useEffect(() => {
+    if (localStorage.getItem("isLoggedIn") !== null) {
+      localStorage.removeItem("isLoggedIn");
+    }
+    if (localStorage.getItem("users") === null) {
+      return;
+    } else {
+      const items = JSON.parse(localStorage.getItem("users"));
+      setUsers([...items]);
+    }
+  }, []);
 
   return (
     <UserContext.Provider
       value={{
+        users: users,
         isLoggedIn: isLoggedIn,
+        userExist: userExist,
+        incorrectInfo: incorrectInfo,
+        onRegister: newUserHandler,
         onLogIn: logInHandler,
         onLogout: logoutHandler,
       }}
