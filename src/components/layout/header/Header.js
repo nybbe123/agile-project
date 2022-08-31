01fd7e33650418058678fe10967e85a7c7aaceaf
Fia <94110257+fiababiakandersson@users.noreply.github.com>
2022-08-31 15:54:59 +0200

    Made so that menu switches, still have some styling left todo
    Co-authored-by: Ella Larsson <EllaMiri@users.noreply.github.com>

--- src/components/layout/header/Header.js
+++ src/components/layout/header/Header.js
@@ -1,13 +1,33 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import UserContext from "../../../context/UserContext";
-import style from "./Header.module.css";
 import { Link as Scroll } from "react-scroll";
 import burgerMenu from "../../../assets/burger-menu.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faXmark
+} from "@fortawesome/free-solid-svg-icons";
+import style from "./Header.module.css";
 
 const Header = () => {
   const ctx = useContext(UserContext);
   const navigate = useNavigate();
+
+ const menuToggle = () => {
+  const menu = document.getElementById('openedMenu');
+  const hamMenu = document.getElementById('hamMenu');
+  const close = document.getElementById('closeMenu');
+  if ( menu.style.display !== 'flex') {
+     menu.style.display = 'flex';
+     menu.classList.add(style["openMenuStyle"]);
+     hamMenu.style.display = 'none';
+     close.style.display = 'flex';
+  } else {
+    menu.style.display = 'none';
+    close.style.display = 'none';
+    hamMenu.style.display = 'flex';
+  }
+}
 
   return (
     <header className={style.header}>
@@ -72,7 +92,51 @@ const Header = () => {
           )}
         </ul>
       </nav>
-      <img src={burgerMenu} alt="burgerMenu" className={style.burgerMenu} />
+      <div id="openedMenu" className={[style.openedMenuClass]}>
+        <ul>
+          <li>SERVICES</li>
+          <li>ABOUT US</li>
+          <li>CONTACT</li>
+          {ctx.isLoggedIn ? (
+            <li
+              className={[style.loginOutBtn]}
+              onClick={() => {
+                ctx.onLogout();
+              }}
+            >
+              LOGOUT
+            </li>
+          ) : (
+            <li
+              className={[style]}
+              onClick={() => {
+                navigate("/login");
+              }}
+            >
+              LOGIN
+            </li>
+          )}
+        </ul>
+      </div>
+      <img
+        onClick={menuToggle}
+        src={burgerMenu}
+        alt="burgerMenu"
+        className={style.burgerMenu}
+        id="hamMenu"
+      />
+      <FontAwesomeIcon
+        onClick={menuToggle}
+        id="closeMenu"
+        icon={faXmark}
+        style={{
+          display: "none",
+          fontSize: "1.7rem",
+          marginTop: "1rem",
+          marginRight: "1rem",
+          zIndex: '1',
+        }}
+      />
     </header>
   );
 };
