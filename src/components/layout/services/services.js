bac8de350a55407d8cdd1c903840bac2d93b9247
Erikisak <erik.isak@icloud.com>
2022-09-02 12:12:36 +0200

    responsive edit to service, problems with image alignment

--- src/components/layout/services/services.js
+++ src/components/layout/services/services.js
@@ -20,6 +20,7 @@ const Services = () => {
             <h2 className={style["whtStyle"]}>WE HAVE THE </h2>
             <h2 className={style["expStyle"]}>EXPERIENCE</h2>
           </div>
+          <div className={style["infoAlign"]}>
           <p className={style["infoStyle"]}>
             Custom web application development by our expert web-app developers.
             Front-end & design, backend, database, and server scale designed to
@@ -28,11 +29,14 @@ const Services = () => {
             visual identity of the brand that helps your business establish a
             prominent position on the market.
           </p>
+          </div>
         </div>
+        
         <div className={style["imgAlign"]}>
           <div className={style["box"]}></div>
           <img className={style["image"]} src={serviceImage}></img>
         </div>
+  
       </div>
 
       <div className={style["boxesAlign"]}>
@@ -43,13 +47,15 @@ const Services = () => {
           />
           <div className={style["backEndAlign"]}>
             <h1 className={style["backEnd"]}>BACK-END SERVICES</h1>
-
+            
+            <div className={style["firstText2Align"]}>
             <p className={style["firstTextAlign"]}>
               As part of our backend services, we build applications from the
               ground up. We have experienced developers who work closely with
               you to determine the best approach when developing your
               application.
             </p>
+            </div>
             <div className={style["iconAlign"]}>
               <Link style={{textDecoration: 'none'}} to="/backEnd">
               <span className={style["learn"]}>learn more</span>
@@ -59,14 +65,18 @@ const Services = () => {
           </div>
         </div>
         <div className={style["firstBox"]}>
+       
           <FontAwesomeIcon icon={faDisplay} className={style["tvIcon"]} />
+     
           <div className={style["frontAlign"]}>
             <h1 className={style["backEnd"]}>FRONT-END DEVELOPMENT</h1>
+            <div className={style["firstText2Align"]}>
             <p className={style["secondTextAlign"]}>
               Webly brings a wealth of experience with HTML, CSS and JavaScript
               to any project We are run by engineers who have been working with
               frontend libraries such as React and Svelte since the early days.{" "}
             </p>
+            </div>
             <div className={style["iconAlign"]}>
               <Link style={{textDecoration: 'none'}} to="/frontEnd">
               <span className={style["learn"]}>learn more</span>
@@ -79,11 +89,13 @@ const Services = () => {
           <FontAwesomeIcon icon={faPaintBrush} className={style["brushIcon"]} />
           <div className={style["digitalAlign"]}>
             <h1 className={style["backEnd"]}>DIGITAL DESIGN</h1>
-            <p className={style["firstTextAlign"]}>
+            <div className={style["firstText2Align"]}>
+            <p className={style["thirdTextAlign"]}>
               We create custom design that strengthens your brand identity,
               helps you reach new customers as well as fueling up the user
               experience and expand potential business.
             </p>
+            </div>
             <div className={style["iconAlign"]}>
               <Link style={{textDecoration: 'none'}} to="/digitalDesign">
               <span className={style["learn"]}>learn more</span>
