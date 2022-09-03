f53f9b692a7d98bd9df9555475ecdc666f6e3054
Erikisak <erik.isak@icloud.com>
2022-09-03 23:04:52 +0200

    edit responsive detail pages

--- src/components/layout/detailPages/backEndPage.js
+++ src/components/layout/detailPages/backEndPage.js
@@ -14,6 +14,7 @@ const BackEndPage = () => {
     <div>
       <div className={style["section1"]}>
         <div className={style["backgroundImg"]}>
+      
           <div className={style["navAlign"]}>
             <Link style={{ textDecoration: "none" }} to="/">
               <p className={style["home"]}>HOME</p>
@@ -23,6 +24,7 @@ const BackEndPage = () => {
           </div>
           <h1 className={style["h1"]}>BACK-END DEVELOPMENT</h1>
           <h2 className={style["h2"]}>We back you up</h2>
+
         </div>
       </div>
       <div className={style["section2"]}>
@@ -39,32 +41,37 @@ const BackEndPage = () => {
             />
             <div className={style["backEndAlign"]}>
               <h1 className={style["backEnd"]}>NODE JS</h1>
-
+              <div className={style["alignment"]}>
               <p className={style["firstTextAlign"]}>
                 Webly promises to create a well functioning back end. We build
                 the server using Node JS.
               </p>
+            </div>
             </div>
           </div>
           <div className={style["firstBox"]}>
             <FontAwesomeIcon icon={faDisplay} className={style["tvIcon"]} />
             <div className={style["frontAlign"]}>
               <h1 className={style["backEnd"]}>MONGO DB</h1>
+              <div className={style["alignment"]}>
               <p className={style["firstTextAlign"]}>
                 We use the Mongo DB database so that we safely can store user
                 information like passwords, emails and phone numbers and much
                 more.
               </p>
+              </div>
             </div>
           </div>
           <div className={style["firstBox"]}>
             <FontAwesomeIcon icon={faCode} className={style["brushIcon"]} />
             <div className={style["digitalAlign"]}>
               <h1 className={style["backEnd"]}>BCRYPT</h1>
+              <div className={style["alignment"]}>
               <p className={style["firstTextAlign"]}>
                 Security is a priority for us at Webly, we make sure to encrypt
                 all sensitive information such as passwords.
               </p>
+            </div>
             </div>
           </div>
         </div>
@@ -76,13 +83,16 @@ const BackEndPage = () => {
           <p className={style["head2"]}>our customers</p>
         </div>
         <div className={style["comp1"]}>
+        <div className={style["imageContainer"]}>
           <div className={style["imgAlign"]}>
             <div className={style["box"]}></div>
             <img className={style["image"]} src={serviceImage}></img>
           </div>
+          </div>
 
           <div className={style["comp1TextAlign"]}>
             <p className={style["head2"]}>COMME CI COMME CA</p>
+            <div className={style["alignment"]}>
             <p className={style["text1"]}>
               We built a database for the furniture selling company Comme Ci
               Comme Ca. They can now view customer orders, stock status of their
@@ -90,11 +100,13 @@ const BackEndPage = () => {
               can also register an account to view their orders. The users
               sensitive information is encrypted.
             </p>
+            </div>
           </div>
         </div>
         <div className={style["comp1"]}>
           <div className={style["comp1TextAlign"]}>
             <p className={style["head2"]}>Chatlie</p>
+            <div className={style["alignment"]}>
             <p className={style["text1"]}>
               Webly developed a chat application for the company Chatlie using
               Socket.io technology. The users of the application can create a
@@ -103,10 +115,12 @@ const BackEndPage = () => {
               chat room is typing.
             </p>
           </div>
-
+          </div>
+          <div className={style["imageContainer"]}>
           <div className={style["imgAlign"]}>
             <div className={style["box"]}></div>
-            <img className={style["image"]} src={serviceImage}></img>
+            <img className={style["image1"]} src={serviceImage}></img>
+          </div>
           </div>
         </div>
       </div>
--- src/components/layout/detailPages/designPage.js
+++ src/components/layout/detailPages/designPage.js
@@ -39,7 +39,7 @@ const DesignPage = () => {
             />
             <div className={style["backEndAlign"]}>
               <h1 className={style["backEnd"]}>UX</h1>
-
+              <div className={style["alignment"]}>
               <p className={style["firstTextAlign"]}>
                 The user experience is one of the most important aspects of a
                 working website. We aim to create user friendly websites with
@@ -47,16 +47,19 @@ const DesignPage = () => {
                 visiting your website.
               </p>
             </div>
+            </div>
           </div>
           <div className={style["firstBox"]}>
             <FontAwesomeIcon icon={faPalette} className={style["tvIcon"]} />
             <div className={style["frontAlign"]}>
               <h1 className={style["backEnd"]}>UI</h1>
+              <div className={style["alignment"]}>
               <p className={style["firstTextAlign"]}>
                 The looks of a website is what attracts a user a lot of the
                 time. We take a lot of time developing the design of your
                 website to make it look and feel professional.
               </p>
+            </div>
             </div>
           </div>
           <div className={style["firstBox"]}>
@@ -66,10 +69,12 @@ const DesignPage = () => {
             />
             <div className={style["digitalAlign"]}>
               <h1 className={style["backEnd"]}>PHOTOSHOP</h1>
+              <div className={style["alignment"]}>
               <p className={style["firstTextAlign"]}>
                 Our team has great experience in Adobe programs such as
                 Photoshop to create the best design for your website.
               </p>
+            </div>
             </div>
           </div>
         </div>
@@ -81,13 +86,16 @@ const DesignPage = () => {
           <p className={style["head2"]}>our customers</p>
         </div>
         <div className={style["comp1"]}>
+        <div className={style["imageContainer"]}>
           <div className={style["imgAlign"]}>
             <div className={style["box"]}></div>
             <img className={style["image"]} src={serviceImage}></img>
           </div>
+          </div>
 
           <div className={style["comp1TextAlign"]}>
             <p className={style["head2"]}>PLANET OF SNEAKERS</p>
+            <div className={style["alignment"]}>
             <p className={style["text1"]}>
               Using Adobe Illustrator the Webly team created a graphic profile
               for the company Planet of Sneakers. The graphic profile contains
@@ -96,10 +104,12 @@ const DesignPage = () => {
               wireframes and mockups for desktop, tablet and mobile.
             </p>
           </div>
+          </div>
         </div>
         <div className={style["comp1"]}>
           <div className={style["comp1TextAlign"]}>
             <p className={style["head2"]}>PHARMA</p>
+            <div className={style["alignment"]}>
             <p className={style["text1"]}>
               Webly remade the webpage for the pharmacy company Pharma. The goal
               was to re-construct their page and make their UX design better.
@@ -107,10 +117,12 @@ const DesignPage = () => {
               experience and worked closely with the client.
             </p>
           </div>
-
+          </div>
+          <div className={style["imageContainer"]}>
           <div className={style["imgAlign"]}>
             <div className={style["box"]}></div>
-            <img className={style["image"]} src={serviceImage}></img>
+            <img className={style["image1"]} src={serviceImage}></img>
+          </div>
           </div>
         </div>
       </div>
